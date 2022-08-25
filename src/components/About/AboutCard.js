2a4b45b99f93256fd4a4957e5f6ca22e52b8b48c
Muziwndile <muziwandilenko@gmail.com>
2022-08-25 14:31:23 +0200

    aboutcard.js fille updated

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,10 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Umuziwandile Nkomo </span>
+            from <span className="purple"> Bulawayo, Zimbabwe.</span>
+            <br />I am an experienced cloud architect, blockchain and DevOps engineer
+            with years of experience.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -21,17 +21,17 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> Playing Football
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Participating in Hacker-thornes
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Umuziwandile</footer>
         </blockquote>
       </Card.Body>
     </Card>
