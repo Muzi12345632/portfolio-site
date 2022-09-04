ee6031c338bfb10b7702692e6c406c9c19726261
Muziwandile Nkomo <84638389+Muzi12345632@users.noreply.github.com>
2022-09-04 22:32:25 +0200

    increased page number

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -36,7 +36,7 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
